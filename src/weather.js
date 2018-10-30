88e0c3cd8ef30de6d6aa13e8489e091c52994bbd
Caitlin Cooling <ccooling97@gmail.com>
2018-10-30 15:58:50 +0000

    Refactor

--- src/weather.js
+++ src/weather.js
@@ -1,18 +1,13 @@
 var Weather = function(){
   this.number = 0
+  this.weather = ""
 };
 
 Weather.prototype.getWeather = function () {
   function set() {
     return Math.floor(Math.random() * Math.floor(4))
   };
-
-  console.log(set());
   this.number = set();
-
-  if(this.number > 1){
-    return "Sunny"
-  } else {
-    return "Stormy"
-  }
+  (this.number > 1) ? this.weather = "Sunny" : this.weather = "Stormy"
+  return this.weather
 };
