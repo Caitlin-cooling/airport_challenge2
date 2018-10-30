bd218a810d00c0d0fb53b4106e6e16a5bdd2389a
Caitlin Cooling <ccooling97@gmail.com>
2018-10-30 15:43:26 +0000

    Refactor

--- src/airport.js
+++ src/airport.js
@@ -1,6 +1,8 @@
 var Airport = function(){
+  this.capacity = 10
 };
 
-Airport.prototype.capacity = function (number) {
-  return number || 10
+Airport.prototype.getCapacity = function (number) {
+  this.capacity = number || 10
+  return this.capacity
 };
