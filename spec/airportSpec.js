b39c3a0efe02cc04c447335a93799080932d3790
Caitlin Cooling <ccooling97@gmail.com>
2018-10-30 18:47:01 +0000

    Functionality to check if the hangar is full

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -15,8 +15,28 @@ describe('Airport', function() {
       expect(airport.getCapacity(50)).toBe(50)
     });
 
-    // it('can tell when the airport is full', function() {
-    //   spyOn(airport, "land").and.returnValue(1);
-    // });
+    it('adds a plane to the hangar when it lands', function() {
+      var plane = jasmine.createSpyObj('plane', ['land']);
+      plane.land.and.callFake(function() {
+        return "1";
+      });
+      
+      airport.addPlane(plane)
+      expect(airport.getHangar()).toEqual(1)
+    });
+  });
+
+  describe('isFull', function() {
+    it('returns true when the hangar is full', function() {
+      var plane = jasmine.createSpyObj('plane', ['land']);
+      plane.land.and.callFake(function() {
+        return "1";
+      });
+
+      for (var i = 0; i < 11; i++) {
+        airport.addPlane(plane)
+      }
+      expect(airport.isFull()).toBe(true)
+    });
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,8 +1,21 @@
 var Airport = function(){
-  this.capacity = 10
+  this.capacity = 10;
+  this.hangar = [];
 };
 
 Airport.prototype.getCapacity = function (number) {
   this.capacity = number || 10
   return this.capacity
+};
+
+Airport.prototype.addPlane = function (plane) {
+  this.hangar.push(plane)
+};
+
+Airport.prototype.getHangar = function () {
+  return this.hangar.length
+};
+
+Airport.prototype.isFull = function() {
+  if(this.hangar.length > this.capacity) return true
 };
