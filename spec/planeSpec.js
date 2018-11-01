a6e4229cbf1d1037024844296da47cf02b2ef56c
Caitlin Cooling <ccooling97@gmail.com>
2018-11-01 08:53:46 +0000

    Plane cannot take off if the weather is stormy

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -3,6 +3,13 @@ describe("Plane", function() {
 
   beforeEach(function(){
     plane = new Plane();
+    this.airport = jasmine.createSpyObj('airport', ['getHangar', 'getCapacity']);
+    this.airport.getHangar.and.callFake(function() {return []; });
+    this.airport.getCapacity.and.callFake(function() {return 10; });
+    this.badWeather = jasmine.createSpyObj('weather', ['getWeather']);
+    this.goodWeather = jasmine.createSpyObj('weather', ['getWeather']);
+    this.badWeather.getWeather.and.callFake(function(){return "Stormy"; });
+    this.goodWeather.getWeather.and.callFake(function(){return "Sunny"; });
   })
 
   describe('has a status', function() {
@@ -13,41 +20,42 @@ describe("Plane", function() {
 
   describe('landing a plane', function() {
     it('changes to landed when the plane lands', function() {
-      var airport = jasmine.createSpyObj('airport', ['getHangar', 'getCapacity']);
-       airport.getHangar.and.callFake(function() {
-         return [];
-       });
-       airport.getCapacity.and.callFake(function() {
-         return 10;
-       });
-
-      plane.takeOff()
-      plane.land(airport)
+      var weather = this.goodWeather.getWeather();
+      plane.takeOff(weather)
+      plane.land(this.airport)
       expect(plane.getStatus()).toBe("Landed")
     });
 
     it("won't allow a plane to land if the airport is full", function() {
-     airport = jasmine.createSpyObj('airport', ['getHangar', 'getCapacity']);
-      airport.getHangar.and.callFake(function() {
-        return [];
-      });
-      airport.getCapacity.and.callFake(function() {
-        return 0;
-      });
+      airport = jasmine.createSpyObj('airport', ['getHangar', 'getCapacity']);
+      airport.getHangar.and.callFake(function() {return []; });
+      airport.getCapacity.and.callFake(function() {return 0; });
 
       expect(function(){plane.land(airport)}).toThrow(new Error("Cannot land: airport full"))
+    });
+
+    it('raises an error if the weather is stormy', function() {
+      var weather = this.badWeather.getWeather();
+      expect(function() {plane.takeOff(weather)}).toThrow(new Error('Cannot take off: the weather is stormy'))
     });
   });
 
   describe('taking off a plane', function(){
     it('changes to flying when the plane takes off', function() {
-      plane.takeOff()
+      var weather = this.goodWeather.getWeather();
+      plane.takeOff(weather)
       expect(plane.getStatus()).toBe("Flying")
     });
 
     it('raises error if a flying plane tries to take off', function() {
-      plane.takeOff()
-      expect(function() {plane.takeOff()}).toThrow(new Error("Cannot take off: plane flying"))
+      var weather = this.goodWeather.getWeather();
+      plane.takeOff(weather)
+      expect(function() {plane.takeOff(weather)}).toThrow(new Error("Cannot take off: plane flying"))
+    });
+
+    it('raises an error if the weather is stormy', function() {
+      var weather = this.badWeather.getWeather();
+      expect(function() {plane.takeOff(weather)}).toThrow(new Error('Cannot take off: the weather is stormy'))
     });
   });
 });
--- src/plane.js
+++ src/plane.js
@@ -1,30 +1,37 @@
 var Plane = function(){
   this.status = "Landed"
+  this.weather = "Sunny"
 };
 
 Plane.prototype.getStatus = function () {
   return this.status
 };
 
-Plane.prototype.takeOff = function(airport) {
-  if(this.status == "Landed") {
-    this.status = "Flying"
-  } else {
-    var e = new Error("Cannot take off: plane flying")
-    throw e;
+Plane.prototype.takeOff = function(weather) {
+  if (weather == "Stormy") {
+    var badWeather = new Error('Cannot take off: the weather is stormy')
+    throw badWeather;
+  } else if(this.status == "Landed") {
+      this.status = "Flying"
+  } else if (this.status = "Flying"){
+      var flying = new Error("Cannot take off: plane flying")
+      throw flying;
   };
 };
 
-Plane.prototype.land = function(airport) {
-  var hangar = airport.getHangar()
-  var capacity = airport.getCapacity()
+Plane.prototype.land = function(airport, weather) {
+  var hangar = airport.getHangar();
+  var capacity = airport.getCapacity();
 
   if(hangar.length >= capacity) {
-    var e = new Error("Cannot land: airport full")
-    throw e;
+    var full = new Error("Cannot land: airport full")
+    throw full;
   } else {
     this.status = "Landed"
     hangar.push(this)
-    console.log("hanger after landing", hangar);
+  };
+  if(weather == 'Stormy'){
+    var badWeather = new Error('Cannot take off: the weather is stormy')
+    throw badWeather;
   };
 };
